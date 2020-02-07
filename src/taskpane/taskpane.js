9d17b94955acfd88c6f922ee88338a30870e7ae0
aproulx232 <aproulx@purdue.edu>
2020-02-06 23:10:52 -0500

    Moved logic to functions

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -20,7 +20,27 @@ function getMonday(date) {
   return date;
 }
 
-function buildOutline(activePage) {}
+function buildOutline(today, activePage) {
+  var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  var title = "";
+  for (var day = 0; day < weekDays.length; day++) {
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0");
+
+    if (today.getDay() == 1) {
+      title += mm + "/" + dd + " - ";
+    }
+    if (today.getDay() == 5) {
+      title += mm + "/" + dd;
+    }
+    var html = "<h2>" + weekDays[day] + " " + mm + "/" + dd + "</h2><p>: </p><p>: </p>";
+    var activeOutline = activePage.addOutline(40, 90 + 90 * day, html);
+    activeOutline.paragraphs.getItemAt(1).addNoteTag("ToDo", "Normal");
+    activeOutline.paragraphs.getItemAt(2).addNoteTag("ToDo", "Normal");
+    today.setDate(today.getDate() + 1);
+  }
+  return title;
+}
 
 export async function run() {
   try {
@@ -36,38 +56,7 @@ export async function run() {
       activePage = context.application.getActivePage();
       var today = new Date();
       today = getMonday(today);
-
-      var dd = String(today.getDate()).padStart(2, "0");
-      var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-
-      var title = "" + mm + "/" + dd + " - ";
-      var mondayHtml = "<h2>Monday  " + mm + "/" + dd + "</h2><p>  => </p><p> => </p>";
-      var monOutline = activePage.addOutline(40, 90, mondayHtml);
-      var monParagraphs = monOutline.paragraphs;
-      monParagraphs.getItemAt(1).addNoteTag("ToDo", "Normal");
-      monParagraphs.getItemAt(2).addNoteTag("ToDo", "Normal");
-      // monParagraphs
-      //   .getItemAt(1)
-      //   .paragraphs.getItemAt(0)
-      //   .addNoteTag("ToDo", "Normal");
-
-      today.setDate(today.getDate() + 1);
-      dd = String(today.getDate()).padStart(2, "0");
-      mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-
-      today = mm + "/" + dd;
-      var tuesdayHtml = "<h2>Tuesday</h2><p>...</p><p>...</p>";
-      var tueOutline = activePage.addOutline(40, 180, tuesdayHtml);
-      tueOutline.paragraphs.getItemAt(1).addNoteTag("ToDo", "Normal");
-
-      var wednesdayHtml = "<h2>Wednesday</h2>";
-      activePage.addOutline(40, 270, wednesdayHtml);
-
-      var thursdayHtml = "<h2>Thursday</h2>";
-      activePage.addOutline(40, 360, thursdayHtml);
-
-      var fridayHtml = "<h2>Friday</h2>";
-      activePage.addOutline(40, 450, fridayHtml);
+      var title = buildOutline(today, activePage);
 
       activePage.title = title;
       // Run the queued commands, and return a promise to indicate task completion.
