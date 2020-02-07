16699641a92bff674c977e0f519b3eef531c15ec
aproulx232 <aproulx@purdue.edu>
2020-02-06 22:36:55 -0500

    Add: Date for Monday and title

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -13,6 +13,15 @@ Office.onReady(info => {
   }
 });
 
+function getMonday(date) {
+  while (date.getDay() != 1) {
+    date.setDate(date.getDate() + 1);
+  }
+  return date;
+}
+
+function buildOutline(activePage) {}
+
 export async function run() {
   try {
     await OneNote.run(async context => {
@@ -25,18 +34,31 @@ export async function run() {
 
       context.application.navigateToPage(newPage);
       activePage = context.application.getActivePage();
+      var today = new Date();
+      today = getMonday(today);
 
-      var mondayHtml = "<h2>Monday</h2>";
+      var dd = String(today.getDate()).padStart(2, "0");
+      var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+
+      var title = "" + mm + "/" + dd + " - ";
+      var mondayHtml = "<h2>Monday  " + mm + "/" + dd + "</h2><p>  => </p><p> => </p>";
       var monOutline = activePage.addOutline(40, 90, mondayHtml);
       var monParagraphs = monOutline.paragraphs;
       monParagraphs.getItemAt(1).addNoteTag("ToDo", "Normal");
+      monParagraphs.getItemAt(2).addNoteTag("ToDo", "Normal");
       // monParagraphs
       //   .getItemAt(1)
       //   .paragraphs.getItemAt(0)
       //   .addNoteTag("ToDo", "Normal");
 
-      var tuesdayHtml = "<h2>Tuesday</h2>";
-      activePage.addOutline(40, 180, tuesdayHtml);
+      today.setDate(today.getDate() + 1);
+      dd = String(today.getDate()).padStart(2, "0");
+      mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+
+      today = mm + "/" + dd;
+      var tuesdayHtml = "<h2>Tuesday</h2><p>...</p><p>...</p>";
+      var tueOutline = activePage.addOutline(40, 180, tuesdayHtml);
+      tueOutline.paragraphs.getItemAt(1).addNoteTag("ToDo", "Normal");
 
       var wednesdayHtml = "<h2>Wednesday</h2>";
       activePage.addOutline(40, 270, wednesdayHtml);
@@ -47,6 +69,7 @@ export async function run() {
       var fridayHtml = "<h2>Friday</h2>";
       activePage.addOutline(40, 450, fridayHtml);
 
+      activePage.title = title;
       // Run the queued commands, and return a promise to indicate task completion.
       return context.sync();
     });
